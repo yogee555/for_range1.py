b75bf50660ba9f53abaa4f769c280d61262a82db
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-06-02 12:43:03 +0530

    Add files via upload

---
+++ for_range1.py
@@ -0,0 +1,3 @@
+# start with start value and end value
+for x in range(18,50):                            # start 18 number to 50 number
+    print(x)                                      # print 18 to 50 number
